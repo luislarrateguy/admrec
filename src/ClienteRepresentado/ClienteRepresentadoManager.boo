7c95f7712feff7c341f01ab30697168e14be4bd2
miltondp <miltondp@24aae9ee-7633-0410-8474-b19e06cb3126>
2007-08-26 13:03:02 +0000

    - Agregué la licencia GPLv3 (disculpen el atrevimiento :) - Cuestiones menores.

--- src/ClienteRepresentado/ClienteRepresentadoManager.boo
+++ src/ClienteRepresentado/ClienteRepresentadoManager.boo
@@ -1,7 +1,22 @@
-// ClienteRepresentadoManager.boo creado conh MonoDevelop
-// User: nacho - 15:46 24/08/2007
-//
-//
+/*
+    MensajeroRemoting - Mensajero instantáneo hecho con .NET Remoting
+    y otras tecnologías de .NET.
+    Copyright (C) 2007  Luis Ignacio Larrateguy, Milton Pividori y César Sandrigo
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 namespace ClienteRepresentado
 
 import System
--- src/JavaCliente/Test.java
+++ src/JavaCliente/Test.java
@@ -1,12 +1,22 @@
-// application.java creado conh MonoDevelop
-// User: nacho - 10:37 24/08/2007
-//
-//
 /*
- * application.java
- *
- * created on 24/08/2007 at 10:37
- */
+    MensajeroRemoting - Mensajero instantáneo hecho con .NET Remoting
+    y otras tecnologías de .NET.
+    Copyright (C) 2007  Luis Ignacio Larrateguy, Milton Pividori y César Sandrigo
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 import cli.Gtk.*;
 import cli.Glade.*;
 

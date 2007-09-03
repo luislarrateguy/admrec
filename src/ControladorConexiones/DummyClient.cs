9d40068cf1b01567f80fc353cbd59048eaaef1b4
miltondp <miltondp@24aae9ee-7633-0410-8474-b19e06cb3126>
2007-09-03 11:44:46 +0000

    - Agrego en el proyecto ControladorConexiones una clase DummyClient, para hacer que el ControladorCliente actúe como antes de agregar la clase ICliente, así por ejemplo BigBrother funciona sin modificar.

---
+++ src/ControladorConexiones/DummyClient.cs
@@ -0,0 +1,38 @@
+// DummyClient.cs created with MonoDevelop
+// User: miltondp at 8:36 AM 9/3/2007
+//
+// To change standard headers go to Edit->Preferences->Coding->Standard Headers
+//
+
+using System;
+
+namespace MensajeroRemoting
+{
+	internal class DummyClient : ICliente
+	{
+		public DummyClient()
+		{
+		}
+		
+		public MensajeRecibidoHandler MetodoMensajeRecibido
+		{
+			get {
+				return new MensajeRecibidoHandler(delegate {});
+			}
+		}
+		
+		public ConexionClienteHandler MetodoContactoConectado
+		{
+			get {
+				return new ConexionClienteHandler(delegate {});
+			}
+		}
+		
+		public ConexionClienteHandler MetodoContactoDesconectado
+		{
+			get {
+				return new ConexionClienteHandler(delegate {});
+			}
+		}
+	}
+}

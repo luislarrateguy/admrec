14b88133bc656ba5cd1b8abeec1013f0c42734ae
lilarrateguy <lilarrateguy@24aae9ee-7633-0410-8474-b19e06cb3126>
2007-09-06 19:06:21 +0000

    Agrego el cliente PHP Esta configurado para andar con nachosemantic.no-ip.biz, luego lo cambio (es cambiar el HTML y el PHP por la ip del servidor tanto del WS como web de php) Corre sobre apache+php (copiar bajo cualquier directorio del DocumentRoot) el Webservice corre con XSP (habría que probar lo que dijo Milton con mod_mono pasa ASP.NET). Descomprimir además yui.tar.gz en esa misma carpeta (no subo los archivos individuales porque no se modifica y no tiene sentido)

---
+++ trunk/src/ClientePHP-AJAX/IMWSwrapper.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ *	$Id: wsdlclient1.php,v 1.2 2004/03/15 23:06:17 snichol Exp $
+ *
+ *	WSDL client sample.
+ *
+ *	Service: WSDL
+ *	Payload: document/literal
+ *	Transport: http
+ *	Authentication: none
+ */
+	require_once('nusoap/lib/nusoap.php');
+	$proxyhost = isset($_POST['proxyhost']) ? $_POST['proxyhost'] : '';
+	$proxyport = isset($_POST['proxyport']) ? $_POST['proxyport'] : '';
+	$proxyusername = isset($_POST['proxyusername']) ? $_POST['proxyusername'] : '';
+	$proxypassword = isset($_POST['proxypassword']) ? $_POST['proxypassword'] : '';
+	$client = new soap_client('http://192.168.0.2:8084/index.asmx?wsdl=0', true,
+							$proxyhost, $proxyport, $proxyusername, $proxypassword);
+
+	switch ($_GET["method"]) {
+		case "Conectar": 
+				$param = array('nick' => $_GET['key']);
+				$result = $client->call('Conectar', array('parameters' => $param), '', '', false, true);
+				if (is_string($result["ConectarResult"])) {
+					$result= $result["ConectarResult"] == "true";	
+				} else {
+					$result = array("ERROR");
+				}
+			break;
+		case "Desconectar":
+				$param = array('key' => $_GET['key']);
+				$result = $client->call('Desconectar', array('parameters' => $param), '', '', false, true);
+				if (is_string($result["DesconectarResult"])) {
+					$result= $result["DesconectarResult"] == "true";	
+				} else {
+					$result = array("ERROR");
+				}
+			break;
+		case "GetListaContactos":
+				$param = array('key' => $_GET['key']);
+				$result = $client->call('GetListaContactos', array('parameters' => $param), '', '', false, true);
+				if (is_array($result["GetListaContactosResult"])) {
+					if (is_array($result["GetListaContactosResult"]["anyType"])) {
+						$result = $result["GetListaContactosResult"]["anyType"];
+					} else {
+						$result= array($result["GetListaContactosResult"]["anyType"]);
+					}				
+				} else {
+					if ($result["GetListaContactosResult"] =="")
+						$result = '';
+					else
+						$result = array("ERROR");
+				}
+			break;
+		case "EnviarMensajeA":
+				$param = array('key' =>$_GET['key'],'mensaje'=>$_GET['mensaje'],'nick'=>$_GET['nick']);
+				$result = $client->call('EnviarMensajeA', array('parameters' => $param), '', '', false, true);
+				if (is_string($result["EnviarMensajeAResult"])) {
+					$result= $result["EnviarMensajeAResult"] == "true";	
+				} else {
+					$result = array("ERROR");
+				}
+			break;
+		case "GetUltimosMensajesRecibidos":
+				$param = array('key' => $_GET['key']);
+				$result = $client->call('GetUltimosMensajesRecibidos', array('parameters' => $param), '', '', false, true);
+				if (is_array($result["GetUltimosMensajesRecibidosResult"])) {
+					if (is_array($result["GetUltimosMensajesRecibidosResult"]["anyType"])) {
+						$result = $result["GetUltimosMensajesRecibidosResult"]["anyType"];
+					} else {
+						$result= array($result["GetUltimosMensajesRecibidosResult"]["anyType"]);
+					}				
+				} else {
+					if ($result["GetUltimosMensajesRecibidosResult"] =="")
+						$result = '';
+					else
+						$result = array("ERROR");
+				}
+			break;
+	}
+	echo json_encode($result);
+?>
